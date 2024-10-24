c28d5e7b565ef3990ecab2ae3cf27f7f7c5d414e
Fernando_Mendez2000 <0599191@gmail.com>
2024-10-24 18:18:09 -0400

    R3cycle Beta Testing - Added the Terms of Use page. I attempted to add the diagonal_dark_green.xml & diagnoal_light_green.xml bars like in the figma account, I could not get it to work yet.

--- app/src/main/java/com/example/r3cyclebeta/ResourcesActivity.java
+++ app/src/main/java/com/example/r3cyclebeta/ResourcesActivity.java
@@ -1,12 +1,26 @@
 package com.example.r3cyclebeta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResourcesActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
+
+        Button termsOfUseButton = findViewById(R.id.termsOfUseButton);
+
+        termsOfUseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResourcesActivity.this, TermsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
