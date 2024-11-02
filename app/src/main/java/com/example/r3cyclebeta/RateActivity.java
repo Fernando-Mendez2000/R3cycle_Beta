5f9cb2beec3dbae3f048a589ff902735c65beb28
Tina <156555255+Crivera270@users.noreply.github.com>
2024-11-02 02:17:02 -0400

    Updated buttons and created rate this app page

--- app/src/main/java/com/example/r3cyclebeta/RateActivity.java
+++ app/src/main/java/com/example/r3cyclebeta/RateActivity.java
@@ -1,14 +1,33 @@
 package com.example.r3cyclebeta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RateActivity extends AppCompatActivity {
 
+    private Button communityback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
+
+// Initialize buttons
+        communityback = findViewById(R.id.communityback);
+
+
+
+        // Set listeners for buttons
+        communityback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RateActivity.this, CommunityActivity.class));
+            }
+        });
+
     }
 }
