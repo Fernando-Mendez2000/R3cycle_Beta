54a5b8ee8bcc694e57ac25300f1d61603f64649f
markfowl28 <134896933+markfowl28@users.noreply.github.com>
2024-12-03 16:54:23 -0500

    Add files via upload
    Activity pages for Encyclopedia

--- app/src/main/java/com/example/r3cyclebeta/EncyclopediaActivity.java
+++ app/src/main/java/com/example/r3cyclebeta/EncyclopediaActivity.java
@@ -2,6 +2,9 @@ package com.example.r3cyclebeta;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EncyclopediaActivity extends AppCompatActivity {
@@ -10,5 +13,48 @@ public class EncyclopediaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encyclopedia);
+
+        //initialize buttons
+        Button paperButton = findViewById(R.id.paperButton);
+        Button cardboardButton = findViewById(R.id.cardboardButton);
+        Button glassButton = findViewById(R.id.glassButton);
+        Button plasticsButton = findViewById(R.id.plasticsButton);
+        Button metalButton = findViewById(R.id.metalButton);
+
+        //set listeners for buttons
+        paperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EncyclopediaActivity.this, PaperActivity.class));
+            }
+        });
+
+        cardboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EncyclopediaActivity.this, CardboardActivity.class));
+            }
+        });
+
+        glassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EncyclopediaActivity.this, GlassActivity.class));
+            }
+        });
+
+        plasticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EncyclopediaActivity.this, PlasticsActivity.class));
+            }
+        });
+
+        metalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EncyclopediaActivity.this, MetalActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/r3cyclebeta/PaperActivity.java
@@ -0,0 +1,12 @@
+package com.example.r3cyclebeta;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaperActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paper);
+    }
+}
